d3c717893839c37e0173544cb06f8d095a925839
oladapo <oladapoelijah49@gmail.com>
2024-04-26 19:00:54 +0100

    updated

--- meetup_site/meetups/models.py
+++ meetup_site/meetups/models.py
@@ -24,7 +24,7 @@ class Meetup(models.Model):
     description = models.TextField()
     image = models.ImageField(upload_to='images')
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
-    Participant = models.ManyToManyField(Participant, blank=True, null=True)
+    participants = models.ManyToManyField(Participant, blank=True, null=True)
     
 
     def __str__(self):
